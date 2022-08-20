b1fb75d02e277f29fa53f0af876de4bc7adbf749
kamrahul <hi2rahulk@gmail.com>
2022-08-20 21:53:03 +0530

    Added library for angluar material
    npm install --save @angular/material @angular/cdk @angular/animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+
+https://www.geeksforgeeks.org/what-is-the-difference-between-declarations-providers-and-import-in-ngmodule/#:~:text=Declarations%20are%20used%20to%20make%20directives.,belongs%20to%20the%20current%20module.
+
+
+*/
