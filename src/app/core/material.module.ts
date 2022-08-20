fd9f1a5c7a36d8b69e153534cb3b6aa7068ab755
kamrahul <hi2rahulk@gmail.com>
2022-08-20 22:07:24 +0530

    Aded @ngModule Decorator to make it as module

--- src/app/core/material.module.ts
+++ src/app/core/material.module.ts
@@ -1,3 +1,7 @@
 import {NgModule} from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule} from '@angular/material';
+
+
+
+@NgModule({})
