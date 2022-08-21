5a65465f2d2df583c16708423345e2baa1870899
kamrahul <hi2rahulk@gmail.com>
2022-08-21 21:13:44 +0530

    Import form module in app.module to support form features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     RouterModule,
-    CustomMaterialModule
+    CustomMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
